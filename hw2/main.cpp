2890bd475c3a7f2b8ae8b60947d8639bee8354f6
az0t24 <azat.malm24@gmail.com>
2022-12-03 17:39:27 +0300

    Readme was added

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -6,7 +6,10 @@ int main(int argc, char** argv) {
     std::set<char> nTerm = {'S', 'A'};
     std::set<char> term = {'a', 'b'};
 
-    std::vector<std::string> rules;
+    std::vector<std::string> rules;//    rules.emplace_back("S->AT");
+//    rules.emplace_back("S->AD");
+//    rules.emplace_back("F->AT");
+//    rules.emplace_back("")
     rules.emplace_back("S->a");
 
     Grammar gr('S', nTerm, term, rules);
